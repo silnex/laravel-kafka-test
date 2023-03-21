5a73a9137ec84808fcac71bf32b8f3fea9844695
silnex <silnex@silnex.kr>
2023-03-22 00:52:49 +0900

    publish, consume 명령어 추가

--- routes/console.php
+++ routes/console.php
@@ -1,19 +1,27 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Junges\Kafka\Facades\Kafka;
+use Junges\Kafka\Message\Message;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+Artisan::command('kafka:publish', function () {
+    $message = new Message(
+        headers: ['header-key' => 'header-value'],
+        body: ['key' => 'value'],
+        key: 'kafka key here'
+    );
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+    /** @var \Junges\Kafka\Producers\ProducerBuilder $producer */
+    $producer = Kafka::publishOn('laravel-kafka-topic')->withMessage($message);
+
+    $producer->send();
+});
+
+Artisan::command('kafka:consume', function () {
+    $consumer = Kafka::createConsumer(['laravel-kafka-topic'])
+        ->withHandler(function ($message) {
+            dump($message);
+        })->build();
+
+    $consumer->consume();
+});
